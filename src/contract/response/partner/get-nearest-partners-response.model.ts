5f3cf77c47fa87fa26f8b1665e8ef0f1209a6b42
gonzo <krsticm@ymail.com>
2020-10-06 09:24:42 +0200

    remove file

--- src/contract/response/partner/get-nearest-partners-response.model.ts
+++
@@ -1,9 +0,0 @@
-import { INearestPartners } from '../..';
-
-export class GetNearestPartnersResponse {
-  nearest;
-
-  constructor(nearest: INearestPartners[]) {
-    this.nearest = nearest;
-  }
-}
