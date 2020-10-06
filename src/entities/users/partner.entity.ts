6039f2f054718bef0c6c64f381e7de179d890fff
gonzo <krsticm@ymail.com>
2020-10-06 09:32:37 +0200

    refactoring

--- src/entities/users/partner.entity.ts
+++ src/entities/users/partner.entity.ts
@@ -1,24 +1,34 @@
 import {
   Entity,
-  Index,
+  Index, OneToMany,
   OneToOne,
   Unique,
 } from 'typeorm';
-import { Partner as IPartner } from '../../contract';
+import { PartnerModel, LocationModel } from '../../contract';
 import { BaseEntity } from './base.entity';
 // eslint-disable-next-line import/no-cycle
 import { Location } from '../location/location.entity';
+// eslint-disable-next-line import/no-cycle
+import { ServiceClassEntity } from '../services/service-class.entity';
+import { ServiceClassModel } from '../../contract/models/users/service-class.model';
+
 
 @Entity('partners')
 @Unique('unique_partner_username', ['username'])
 @Unique('unique_partner_email', ['email'])
 @Index('index_partner_username', ['username'])
 @Index('index_partner_email', ['email'])
-export class Partner extends BaseEntity implements IPartner {
+export class Partner extends BaseEntity implements PartnerModel {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @OneToOne((type) => Location, (location) => location.partner,
     {
       cascade: ['insert', 'update'],
     })
-  location: any; // TODO fix this typescript problem, there should be Location entity, not any
+  // eslint-disable-next-line max-len
+  location?: LocationModel; // TODO fix this typescript problem, there should be Location entity, not any
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @OneToMany((type) => ServiceClassEntity, (sc) => sc.partner)
+  // eslint-disable-next-line max-len
+  service_class?: ServiceClassModel[]; // TODO fix this typescript problem, there should be Location entity, not any
 }
